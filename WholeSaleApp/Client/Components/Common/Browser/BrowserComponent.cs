0a408792edd16128e9a59bee9819c294496d4030
Stefan <stefan.mitrovic@comdata.rs>
2023-05-20 23:26:42 +0200

    browser navigation and functionality improvements

--- WholeSaleApp/Client/Components/Common/Browser/BrowserComponent.cs
+++ WholeSaleApp/Client/Components/Common/Browser/BrowserComponent.cs
@@ -1,35 +1,39 @@
 ﻿using System;
+using System.Collections.ObjectModel;
 using System.Linq.Expressions;
 using System.Reflection;
 using System.Text.Json;
 using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
 using MudBlazor;
+using WholeSaleApp.Client.Helpers;
 using WholeSaleApp.Client.Interfaces;
 using WholeSaleApp.Client.Services;
 using WholeSaleApp.Shared.DTOs;
+using static MudBlazor.CategoryTypes;
 
 
 namespace WholeSaleApp.Client.Components.Common.Browser
 {
     public partial class BrowserComponent<T> where T : BaseDto
     {
-        [Inject] 
-        private IGenericRepository<T> Repository{ get; set; }
+        [Inject] private IDialogService DialogService { get; set; }
+        [Inject] private IGenericRepository<T> Repository { get; set; }
 
-        [Inject] 
-        private NavigationManager NavManager { get; set; }
-        private List<T> GridSource { get; set; }
+        [Inject] private NavigationManager NavManager { get; set; }
+        private ObservableCollection<T> GridSource { get; set; }
         private MudDataGrid<T> _dataGrid;
         private Dictionary<PropertyInfo, RenderFragment> columnFragments = new();
 
         protected override void OnInitialized()
         {
-            GridSource = new List<T>();
+            GridSource = new ObservableCollection<T>();
             base.OnInitialized();
         }
+
         protected override async Task OnInitializedAsync()
         {
-            GridSource = await Repository.GetAsync();
+            GridSource = new ObservableCollection<T>(await Repository.GetAsync());
             await base.OnInitializedAsync();
         }
 
@@ -40,15 +44,31 @@ namespace WholeSaleApp.Client.Components.Common.Browser
 
         private RenderFragment CreateComponent(PropertyInfo propInfo) => builder =>
         {
+            //to be replaced by this, to be analyzed
+            //    // Define our instance parameter, which will be the input of the Func
+            //    var objParameterExpr = Expression.Parameter(typeof(T), "instance");
+            //    // 1. Cast the instance to the correct type
+            //    var instanceExpr = Expression.TypeAs(objParameterExpr, property.DeclaringType);
+            //    // 2. Call the getter and retrieve the value of the property
+            //    var propertyExpr = Expression.Property(instanceExpr, property);
+            //    // 3. Convert the property's value to object
+            //    var propertyObjExpr = Expression.Convert(propertyExpr, typeof(U));
+            //    // Create a lambda expression of the latest call & compile it
+            //    return Expression.Lambda<Func<T, U>>(propertyObjExpr, objParameterExpr);
+
+
             var parameterExpression = Expression.Parameter(typeof(T), "x");
             var propertyAccess = Expression.Property(parameterExpression, propInfo.Name);
-            var lambdaMethod = typeof(Expression).GetMethod("Lambda", 1, new[] { typeof(Expression), typeof(ParameterExpression[]) }, new ParameterModifier[] { new(2) });
+            var lambdaMethod = typeof(Expression).GetMethod("Lambda", 1,
+                new[] { typeof(Expression), typeof(ParameterExpression[]) }, new ParameterModifier[] { new(2) });
             var func = typeof(Func<,>).MakeGenericType(typeof(T), propInfo.PropertyType);
             var lambdaMethodInstance = lambdaMethod.MakeGenericMethod(func);
-            var expression = lambdaMethodInstance.Invoke(this, new object[] { propertyAccess, new[] { parameterExpression } });
+            var expression =
+                lambdaMethodInstance.Invoke(this, new object[] { propertyAccess, new[] { parameterExpression } });
 
 
-            builder.OpenComponent(0, typeof(PropertyColumn<,>).MakeGenericType(new[] { typeof(T), propInfo.PropertyType }));
+            builder.OpenComponent(0,
+                typeof(PropertyColumn<,>).MakeGenericType(new[] { typeof(T), propInfo.PropertyType }));
             builder.AddAttribute(1, "Property", expression);
             builder.CloseComponent();
 
@@ -64,28 +84,79 @@ namespace WholeSaleApp.Client.Components.Common.Browser
             else
             {
                 var newRenderFragment = CreateComponent(property);
-                columnFragments.Add(property,newRenderFragment);
-                return  newRenderFragment;
+                columnFragments.Add(property, newRenderFragment);
+                return newRenderFragment;
             }
         }
 
         private async Task<GridData<T>> ServerData(GridState<T> gridState)
         {
             var items = await Repository.GetAsync();
-        //    var filterFunctions = gridState.FilterDefinitions.Select(x => x.GenerateFilterFunction());
+            //    var filterFunctions = gridState.FilterDefinitions.Select(x => x.GenerateFilterFunction());
 
-            var json = JsonSerializer.Serialize(gridState);
+            //var json = JsonSerializer.Serialize(gridState);
 
-            return new GridData<T>() {Items = items};
+            return new GridData<T>() { Items = items };
         }
 
-        private void RowClicked(DataGridRowClickEventArgs<T> tArgs)
+        private void RowClicked(DataGridRowClickEventArgs<T> arg)
         {
-            if (tArgs.MouseEventArgs.Detail > 1)
+            if (arg.MouseEventArgs.Detail > 1)
             {
-                NavManager.NavigateTo($"/Locations/{tArgs.Item.Id}");
+                OpenEdit(arg.Item.Id);
+            }
+            _dataGrid.SelectedItem = arg.Item;
+        }
+
+        private void ToolBarOpenNew(MouseEventArgs arg)
+        {
+            NavManager.NavigateTo($"/{UrlResolver.GetUrlSectionForType<T>()}/New");
+        }
+
+        private void ToolBarOpenEdit()
+        {
+            if (_dataGrid.SelectedItem != null)
+            {
+                OpenEdit(_dataGrid.SelectedItem.Id);
             }
         }
-    }
 
+        private void ToolBarDelete()
+        {
+            if (_dataGrid.SelectedItem != null)
+            {
+                Delete(_dataGrid.SelectedItem.Id);
+            }
+        }
+
+        private void OpenEdit(int id)
+        {
+
+            NavManager.NavigateTo($"/{UrlResolver.GetUrlSectionForType<T>()}/{id}");
+        }
+
+        private async Task Delete(int id)
+        {
+            bool? result = await DialogService.ShowMessageBox(
+                "Brisanje podatka",
+                "Da li ste sigurni da želite obrisati odabrani podatak? ",
+                yesText: "Obriši", cancelText: "Otkaži");
+
+            if (result == true)
+            {
+                await Repository.DeleteAsync(id);
+                await _dataGrid.ReloadServerData();
+            }
+        }
+
+        private string SelectedRowClassFunc(T item, int rowIndex)
+        {
+            if (_dataGrid.SelectedItem?.Id == item.Id)
+                return " selected ";
+
+            return "";
+        }
+
+        private EventCallback<List<T>> GridSourceChanged { get; set; }
+    }
 }
