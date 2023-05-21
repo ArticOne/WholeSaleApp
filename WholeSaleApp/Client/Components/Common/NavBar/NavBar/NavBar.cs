1c56baa048fb62dd7a5c67c625640cb19b88ce36
Stefan <stefan.mitrovic@comdata.rs>
2023-05-21 20:28:56 +0200

    sub menu seems done

--- WholeSaleApp/Client/Components/Common/NavBar/NavBar/NavBar.cs
+++ WholeSaleApp/Client/Components/Common/NavBar/NavBar/NavBar.cs
@@ -4,21 +4,6 @@ namespace WholeSaleApp.Client.Components.Common.NavBar.NavBar
 {
     public partial class NavBar
     {
-        bool open = false;
-        void ToggleDrawer()
-        {
-            open = !open;
-        }
-        private string SubNavOpenClass = "";
-
-        private void OpenSubMenu()
-        {
-            SubNavOpenClass = " open ";
-        }
-
-        private void CloseSubMenu()
-        {
-            SubNavOpenClass = "";
-        }
+        bool open = true;
     }
 }
--- WholeSaleApp/Client/Components/Common/NavBar/SubNavComponent/SubNavComponent.cs
+++ WholeSaleApp/Client/Components/Common/NavBar/SubNavComponent/SubNavComponent.cs
@@ -1,6 +1,9 @@
-﻿namespace WholeSaleApp.Client.Components.Common.NavBar.SubNavComponent
+﻿using Microsoft.AspNetCore.Components;
+using MudBlazor;
+
+namespace WholeSaleApp.Client.Components.Common.NavBar.SubNavComponent
 {
-    public partial class SubNavComponent
+    public partial class SubNavComponent : MudComponentBase
     {
     }
 }
